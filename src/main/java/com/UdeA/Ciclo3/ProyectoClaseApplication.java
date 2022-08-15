3c4e0fdea0372162715adcdb4ceb9a0235f70f93
Natali Velasquez <natalivelasquezm@gmail.com>
2022-08-15 12:20:51 -0500

    SpringBoot

---
+++ src/main/java/com/UdeA/Ciclo3/ProyectoClaseApplication.java
@@ -0,0 +1,13 @@
+package com.UdeA.Ciclo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoClaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoClaseApplication.class, args);
+	}
+
+}
