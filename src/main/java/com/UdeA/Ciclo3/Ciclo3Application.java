940cd77517629fe3f80449d0e183454d1b193f47
Natali Velasquez <ozzyta1@gmail.com>
2022-08-16 08:08:24 -0500

    Hola mundo!

--- src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
+++ src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
@@ -1,10 +1,20 @@
 package com.UdeA.Ciclo3;
 
+import org.hibernate.annotations.GeneratorType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication (exclude ={SecurityAutoConfiguration.class})
 public class Ciclo3Application {
+
+	@GetMapping("/hello")
+	public String hello(){
+		return "Hola Ciclo 3... Saldremos vivos de esto!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo3Application.class, args);
