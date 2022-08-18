eb823231920ec4af3142990e990d82676b52db59
Natali Velasquez <ozzyta1@gmail.com>
2022-08-18 06:33:07 -0500

    Crear Entidad Empleado

--- src/main/java/com/UdeA/Ciclo3/modelos/Empleado.java
+++ src/main/java/com/UdeA/Ciclo3/modelos/Empleado.java
@@ -1,4 +1,68 @@
 package com.UdeA.Ciclo3.modelos;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="Empleado")
 public class Empleado {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int id;
+    private String nombre;
+    private String correo;
+    @ManyToOne
+    @JoinColumn(name = "empresa_id")
+    private Empresa empresa;
+    private String rol;
+
+    public Empleado() {
+    }
+
+    public Empleado(String nombre, String correo, Empresa empresa, String rol) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.empresa = empresa;
+        this.rol = rol;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
+
 }
