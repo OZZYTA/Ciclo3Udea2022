cd415be89efcda3ca31da476361878f109a8aeec
Natali Velasquez <ozzyta1@gmail.com>
2022-08-18 08:03:15 -0500

    Creacion de repositorios y Servicios de Empresa

--- src/main/java/com/UdeA/Ciclo3/modelos/MovimientoDinero.java
+++ src/main/java/com/UdeA/Ciclo3/modelos/MovimientoDinero.java
@@ -1,4 +1,57 @@
 package com.UdeA.Ciclo3.modelos;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="Movimientos")
 public class MovimientoDinero {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private long monto;
+    private String concepto;
+    @ManyToOne
+    @JoinColumn(name = "empleado_id")
+    private Empleado usuario;
+
+    public MovimientoDinero() {
+    }
+
+    public MovimientoDinero(long monto, String concepto, Empleado empleado) {
+        this.monto = monto;
+        this.concepto = concepto;
+        this.usuario = empleado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public long getMonto() {
+        return monto;
+    }
+
+    public void setMonto(long monto) {
+        this.monto = monto;
+    }
+
+    public String getConcepto() {
+        return concepto;
+    }
+
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+
+    public Empleado getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Empleado empleado) {
+        this.usuario = empleado;
+    }
 }
