02d715a8601e9fffc6db7405fc88c24dac8f4a57
Natali Velasquez <ozzyta1@gmail.com>
2022-08-24 07:36:44 -0500

    Template y servicios para Agregar nueva empresa

--- src/main/java/com/UdeA/Ciclo3/controller/ControllerFull.java
+++ src/main/java/com/UdeA/Ciclo3/controller/ControllerFull.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -28,6 +30,15 @@ public class ControllerFull {
         model.addAttribute("emp",emp);
         return "agregarEmpresa";
     }
+
+    @PostMapping("/GuardarEmpresa")
+    public String guardarEmpresa(Empresa emp, RedirectAttributes redirectAttributes){
+        if(empresaService.saveOrUpdateEmpresa(emp)==true){
+            return "redirect:/VerEmpresas";
+        }
+        return "redirect:/AgregarEmpresa";
+    }
+
 
 
 
