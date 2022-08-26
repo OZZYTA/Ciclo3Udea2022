bc9be009f42d18ab89cabea25c7e2a46e9796185
Natali Velasquez <ozzyta1@gmail.com>
2022-08-26 07:33:03 -0500

    Generamos notifaciones en pantalla despues del CRUD

--- src/main/java/com/UdeA/Ciclo3/controller/ControllerFull.java
+++ src/main/java/com/UdeA/Ciclo3/controller/ControllerFull.java
@@ -16,52 +16,59 @@ public class ControllerFull {
     EmpresaService empresaService;
 
     @GetMapping ({"/","/VerEmpresas"})
-    public String viewEmpresas(Model model){
+    public String viewEmpresas(Model model, @ModelAttribute("mensaje") String mensaje){
         List<Empresa> listaEmpresas=empresaService.getAllEmpresas();
         model.addAttribute("emplist",listaEmpresas);
+        model.addAttribute("mensaje",mensaje);
         return "verEmpresas"; //Llamamos al HTML
     }
 
     @GetMapping("/AgregarEmpresa")
-    public String nuevaEmpresa(Model model){
+    public String nuevaEmpresa(Model model, @ModelAttribute("mensaje") String mensaje){
         Empresa emp= new Empresa();
         model.addAttribute("emp",emp);
+        model.addAttribute("mensaje",mensaje);
         return "agregarEmpresa";
     }
 
     @PostMapping("/GuardarEmpresa")
-    public String guardarEmpresa(Empresa emp){
+    public String guardarEmpresa(Empresa emp, RedirectAttributes redirectAttributes){
         if(empresaService.saveOrUpdateEmpresa(emp)==true){
+            redirectAttributes.addFlashAttribute("mensaje","saveOK");
             return "redirect:/VerEmpresas";
         }
+        redirectAttributes.addFlashAttribute("mensaje","saveError");
         return "redirect:/AgregarEmpresa";
     }
 
     @GetMapping("/EditarEmpresa/{id}")
-    public String editarEmpresa(Model model, @PathVariable Integer id){
+    public String editarEmpresa(Model model, @PathVariable Integer id, @ModelAttribute("mensaje") String mensaje){
         Empresa emp=empresaService.getEmpresaById(id);
         //Creamos un atributo para el modelo, que se llame igualmente emp y es el que ira al html para llenar o alimentar campos
         model.addAttribute("emp",emp);
+        model.addAttribute("mensaje", mensaje);
         return "editarEmpresa";
     }
 
 
     @PostMapping("/ActualizarEmpresa")
-    public String updateEmpresa(@ModelAttribute("emp") Empresa emp){
+    public String updateEmpresa(@ModelAttribute("emp") Empresa emp, RedirectAttributes redirectAttributes){
         if(empresaService.saveOrUpdateEmpresa(emp)){
+            redirectAttributes.addFlashAttribute("mensaje","updateOK");
             return "redirect:/VerEmpresas";
         }
+        redirectAttributes.addFlashAttribute("mensaje","updateError");
         return "redirect:/EditarEmpresa";
 
     }
 
     @GetMapping("/EliminarEmpresa/{id}")
-    public String eliminarEmpresa(@PathVariable Integer id){
-        try{
-            empresaService.deleteEmpresa(id);
-        }catch (Exception e){
+    public String eliminarEmpresa(@PathVariable Integer id, RedirectAttributes redirectAttributes){
+        if (empresaService.deleteEmpresa(id)==true){
+            redirectAttributes.addFlashAttribute("mensaje","deleteOK");
             return "redirect:/VerEmpresas";
         }
+        redirectAttributes.addFlashAttribute("mensaje", "deleteError");
         return "redirect:/VerEmpresas";
     }
 
--- src/main/java/com/UdeA/Ciclo3/service/EmpresaService.java
+++ src/main/java/com/UdeA/Ciclo3/service/EmpresaService.java
@@ -36,11 +36,12 @@ public class EmpresaService {
 
     //Metodo para eliminar empresas registradas teniendo el id
     public boolean deleteEmpresa(Integer id){
-        empresaRepository.deleteById(id);
-        if (getEmpresaById(id)!=null){
-            return false;
+        empresaRepository.deleteById(id);  //Eliminar
+
+        if (empresaRepository.findById(id)!=null){  //Verificacion del servicio eliminacion
+            return true;
         }
-        return true;
+        return false;
     }
 
 }
