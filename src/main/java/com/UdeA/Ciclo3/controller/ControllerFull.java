5dabbd18257e522cee3856cbe407bfd0f613455c
Natali Velasquez <ozzyta1@gmail.com>
2022-08-31 08:11:34 -0500

    Controladores Rest, Nueva Rama

--- src/main/java/com/UdeA/Ciclo3/controller/ControllerFull.java
+++ src/main/java/com/UdeA/Ciclo3/controller/ControllerFull.java
@@ -1,17 +1,25 @@
 package com.UdeA.Ciclo3.controller;
 
 
+import com.UdeA.Ciclo3.modelos.Empleado;
 import com.UdeA.Ciclo3.modelos.Empresa;
+import com.UdeA.Ciclo3.service.EmpleadoService;
 import com.UdeA.Ciclo3.service.EmpresaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 @RestController
 public class ControllerFull {
     @Autowired
     EmpresaService  empresaService;
+    @Autowired
+    EmpleadoService empleadoService;
 
+    //EMPRESAS
     @GetMapping("/enterprises") //Ver json de todas las empresas
     public List<Empresa> verEmpresas(){
         return empresaService.getAllEmpresas();
@@ -48,5 +56,26 @@ public class ControllerFull {
             return "No se pudo eliminar la empresa con id"+id;
         }
     }
+
+    //EMPLEADOS
+    @GetMapping("/empleados") //Ver json de todas los empleados
+    public List<Empleado> verEmpleados(){
+        return empleadoService.getAllEmpleado();
+    }
+
+    @PostMapping("/empleados") //Guardar un empleado nuevo
+    public Optional<Empleado> guardarEmpleado(@RequestBody Empleado empl){
+        return Optional.ofNullable(this.empleadoService.saveOrUpdateEmpleado(empl));
+    }
+    @GetMapping(path = "empleados/{id}")//Consultar empleado por ID
+    public Optional<Empleado> empleadoPorID(@PathVariable("id") Integer id){
+        return this.empleadoService.getEmpleadoById(id);
+    }
+
+    @GetMapping("/enterprises/{id}/empleados")// Consultar empleados por empresa
+    public ArrayList<Empleado> EmpleadoPorEmpresa(@PathVariable("id") Integer id){
+        return this.empleadoService.obtenerPorEmpresa(id);
+    }
+
 
 }
